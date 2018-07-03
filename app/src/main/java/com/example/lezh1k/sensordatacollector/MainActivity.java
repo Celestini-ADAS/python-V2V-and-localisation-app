1922e45e1fb6d346900df91f31be5b5430287278
Sidharth Talia <sidharthtalia@ymail.com>
2018-07-03 21:40:24 +0530

    trying usb communications

--- app/src/main/java/com/example/lezh1k/sensordatacollector/MainActivity.java
+++ app/src/main/java/com/example/lezh1k/sensordatacollector/MainActivity.java
@@ -1,7 +1,13 @@
 package com.example.lezh1k.sensordatacollector;
 
 import android.Manifest;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.ComponentName;
 import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.ServiceConnection;
 import android.content.pm.PackageManager;
 import android.hardware.SensorManager;
 import android.location.Location;
@@ -9,6 +15,8 @@ import android.location.LocationManager;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Environment;
+import android.os.IBinder;
+import android.os.Message;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
@@ -21,6 +29,12 @@ import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.TextView;
+import android.hardware.usb.UsbManager;
+import android.hardware.usb.UsbDevice;
+import android.hardware.usb.UsbDeviceConnection;
+import android.widget.Toast;
+import android.os.Handler;
+
 
 import com.elvishew.xlog.LogLevel;
 import com.elvishew.xlog.XLog;
@@ -31,6 +45,8 @@ import com.elvishew.xlog.printer.file.backup.FileSizeBackupStrategy;
 import com.elvishew.xlog.printer.file.naming.FileNameGenerator;
 import com.example.lezh1k.sensordatacollector.Interfaces.MapInterface;
 import com.example.lezh1k.sensordatacollector.Presenters.MapPresenter;
+import com.felhr.usbserial.UsbSerialDevice;
+import com.felhr.usbserial.UsbSerialInterface;
 import com.mapbox.mapboxsdk.Mapbox;
 import com.mapbox.mapboxsdk.annotations.Polyline;
 import com.mapbox.mapboxsdk.annotations.PolylineOptions;
@@ -41,10 +57,14 @@ import com.mapbox.mapboxsdk.maps.MapView;
 import com.mapbox.mapboxsdk.maps.MapboxMap;
 
 import java.io.File;
+import java.lang.ref.WeakReference;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Locale;
+import java.util.Map;
+import java.util.Set;
 import java.util.TimeZone;
 
 import mad.location.manager.lib.Commons.Utils;
@@ -58,6 +78,48 @@ import mad.location.manager.lib.Services.ServicesHelper;
 
 
 public class MainActivity extends AppCompatActivity implements LocationServiceInterface, MapInterface, ILogger {
+    private final BroadcastReceiver mUsbReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            switch (intent.getAction()) {
+                case UsbService.ACTION_USB_PERMISSION_GRANTED: // USB PERMISSION GRANTED
+                    Toast.makeText(context, "USB Ready", Toast.LENGTH_SHORT).show();
+                    break;
+                case UsbService.ACTION_USB_PERMISSION_NOT_GRANTED: // USB PERMISSION NOT GRANTED
+                    Toast.makeText(context, "USB Permission not granted", Toast.LENGTH_SHORT).show();
+                    break;
+                case UsbService.ACTION_NO_USB: // NO USB CONNECTED
+                    Toast.makeText(context, "No USB connected", Toast.LENGTH_SHORT).show();
+                    break;
+                case UsbService.ACTION_USB_DISCONNECTED: // USB DISCONNECTED
+                    Toast.makeText(context, "USB disconnected", Toast.LENGTH_SHORT).show();
+                    break;
+                case UsbService.ACTION_USB_NOT_SUPPORTED: // USB NOT SUPPORTED
+                    Toast.makeText(context, "USB device not supported", Toast.LENGTH_SHORT).show();
+                    break;
+            }
+        }
+    };
+    private UsbService usbService;
+    private MyHandler mHandler;
+    private  final ServiceConnection usbConnection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName arg0, IBinder arg1) {
+            usbService = ((UsbService.UsbBinder)arg1).getService();
+            usbService.setHandler(mHandler);
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName componentName) {
+            usbService = null;
+        }
+    };
+
+
+
+
+
+
     private String xLogFolderPath;
     int flag=0;
     public long timedel,now;
@@ -142,6 +204,11 @@ public class MainActivity extends AppCompatActivity implements LocationServiceIn
                         m_geoHashRTFilter.speed,
                         m_geoHashRTFilter.direction
                 ));
+                String message = String.format("%.8f%.8f%f%f",m_geoHashRTFilter.displayPoint.Longitude,
+                        m_geoHashRTFilter.displayPoint.Latitude,
+                        m_geoHashRTFilter.speed,
+                        m_geoHashRTFilter.direction);
+                usbService.write(message.getBytes());
             } else {
                 if (!m_sensorCalibrator.isInProgress())
                     return;
@@ -416,6 +483,8 @@ public class MainActivity extends AppCompatActivity implements LocationServiceIn
         } else {
             //todo set some status
         }
+        mHandler = new MyHandler(this);
+        usbService.changeBaudRate(115200);
     }
 
     @Override
@@ -445,6 +514,8 @@ public class MainActivity extends AppCompatActivity implements LocationServiceIn
     @Override
     protected void onResume() {
         super.onResume();
+        setFilters();
+        startService(UsbService.class,usbConnection, null);
         if (m_mapView != null) {
             m_mapView.onResume();
         }
@@ -459,7 +530,8 @@ public class MainActivity extends AppCompatActivity implements LocationServiceIn
         if (m_mapView != null) {
             m_mapView.onPause();
         }
-
+        unregisterReceiver(mUsbReceiver);
+        unbindService(usbConnection);
         m_refreshTask.needTerminate = true;
         m_refreshTask.cancel(true);
         if (m_sensorCalibrator != null) {
@@ -489,5 +561,58 @@ public class MainActivity extends AppCompatActivity implements LocationServiceIn
         if (m_mapView != null) {
             m_mapView.onDestroy();
         }
+    }
+    private static class MyHandler extends Handler {
+        private final WeakReference<MainActivity> mActivity;
+
+        public MyHandler(MainActivity activity) {
+            mActivity = new WeakReference<>(activity);
+        }
+
+//        @Override
+//        public void handleMessage(Message msg) {
+//            switch (msg.what) {
+//                case UsbService.MESSAGE_FROM_SERIAL_PORT:
+//                    String data = (String) msg.obj;
+//                    mActivity.get().display.append(data);
+//                    break;
+//                case UsbService.CTS_CHANGE:
+//                    Toast.makeText(mActivity.get(), "CTS_CHANGE",Toast.LENGTH_LONG).show();
+//                    break;
+//                case UsbService.DSR_CHANGE:
+//                    Toast.makeText(mActivity.get(), "DSR_CHANGE",Toast.LENGTH_LONG).show();
+//                    break;
+//                case UsbService.SYNC_READ:
+//                    String buffer = (String) msg.obj;
+//                    mActivity.get().display.append(buffer);
+//                    break;
+//            }
+//        }
+    }
+
+    private void startService(Class<?> service, ServiceConnection serviceConnection, Bundle extras) {
+        if (!UsbService.SERVICE_CONNECTED) {
+            Intent startService = new Intent(this, service);
+            if (extras != null && !extras.isEmpty()) {
+                Set<String> keys = extras.keySet();
+                for (String key : keys) {
+                    String extra = extras.getString(key);
+                    startService.putExtra(key, extra);
+                }
+            }
+            startService(startService);
+        }
+        Intent bindingIntent = new Intent(this, service);
+        bindService(bindingIntent, serviceConnection, Context.BIND_AUTO_CREATE);
+    }
+
+    private void setFilters() {
+        IntentFilter filter = new IntentFilter();
+        filter.addAction(UsbService.ACTION_USB_PERMISSION_GRANTED);
+        filter.addAction(UsbService.ACTION_NO_USB);
+        filter.addAction(UsbService.ACTION_USB_DISCONNECTED);
+        filter.addAction(UsbService.ACTION_USB_NOT_SUPPORTED);
+        filter.addAction(UsbService.ACTION_USB_PERMISSION_NOT_GRANTED);
+        registerReceiver(mUsbReceiver, filter);
     }
 }
